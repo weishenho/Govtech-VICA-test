07377c0bf7bea2940619d1012c04423ac1f7a44c
Wei Shen <weishen1990@gmail.com>
2023-01-11 02:09:22 +0800

    add cors and casl plugin

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import { accessibleRecordsPlugin } from "@casl/mongoose";
+import "express-async-errors";
+import cors from "cors";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+mongoose.plugin(accessibleRecordsPlugin);
 app.use(express.json());
+app.use(cors());
 mongoose.connect("mongodb://localhost:27017/govtech");
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
