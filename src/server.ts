db0093bbc4d2ccf60f1d84905c0732326758847d
Wei Shen <weishen1990@gmail.com>
2023-01-11 01:21:31 +0800

    connection to mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -7,6 +8,12 @@ const app: Express = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+mongoose.connect("mongodb://localhost:27017/govtech");
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected to MongoDB successfully");
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
